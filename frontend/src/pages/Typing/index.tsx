8314b4a5dd48731d851353c63becdef4edf585cf
Washington <was.antonio.xy@gmail.com>
2025-03-16 17:41:11 -0400

    refactor: removing duplicated code using mapping

--- frontend/src/pages/Typing/index.tsx
+++ frontend/src/pages/Typing/index.tsx
@@ -20,65 +20,35 @@ export interface WindowsKeyboardType {
   height: number;
 }
 
+const KYEBOARD_LINES = [
+  WINDOWS_KEYBOARD_DATA_FIRST_LINE,
+  WINDOWS_KEYBOARD_DATA_SECOND_LINE,
+  WINDOWS_KEYBOARD_DATA_THIRD_LINE,
+  WINDOWS_KEYBOARD_DATA_FOURTH_LINE,
+  WINDOWS_KEYBOARD_DATA_FIFTH_LINE,
+  WINDOWS_KEYBOARD_DATA_SIXTH_LINE,
+]
+
 export function Typing() {
 
   return (
     <PageContainer>
       <main>
-        <KeyboardLineContainer>
-          {
-            WINDOWS_KEYBOARD_DATA_FIRST_LINE.map((keyboardItem: WindowsKeyboardType) => {
-              return (
-                <KeyboardItem {...keyboardItem} />
-              )
-            })
-          }
-        </KeyboardLineContainer>
-        <KeyboardLineContainer>
-          {
-            WINDOWS_KEYBOARD_DATA_SECOND_LINE.map((keyboardItem: WindowsKeyboardType) => {
-              return (
-                <KeyboardItem {...keyboardItem} />
-              )
-            })
-          }
-        </KeyboardLineContainer>
-        <KeyboardLineContainer>
-          {
-            WINDOWS_KEYBOARD_DATA_THIRD_LINE.map((keyboardItem: WindowsKeyboardType) => {
-              return (
-                <KeyboardItem {...keyboardItem} />
-              )
-            })
-          }
-        </KeyboardLineContainer>
-        <KeyboardLineContainer>
-          {
-            WINDOWS_KEYBOARD_DATA_FOURTH_LINE.map((keyboardItem: WindowsKeyboardType) => {
-              return (
-                <KeyboardItem {...keyboardItem} />
-              )
-            })
-          }
-        </KeyboardLineContainer>
-        <KeyboardLineContainer>
-          {
-            WINDOWS_KEYBOARD_DATA_FIFTH_LINE.map((keyboardItem: WindowsKeyboardType) => {
-              return (
-                <KeyboardItem {...keyboardItem} />
-              )
-            })
-          }
-        </KeyboardLineContainer>
-        <KeyboardLineContainer>
-          {
-            WINDOWS_KEYBOARD_DATA_SIXTH_LINE.map((keyboardItem: WindowsKeyboardType) => {
-              return (
-                <KeyboardItem {...keyboardItem} />
-              )
-            })
-          }
-        </KeyboardLineContainer>
+        {
+          KYEBOARD_LINES.map((keyboardLine: WindowsKeyboardType[]) => {
+            return (
+              <KeyboardLineContainer>
+                {
+                  keyboardLine.map((keyboardItem: WindowsKeyboardType) => {
+                    return (
+                      <KeyboardItem {...keyboardItem} />
+                    )
+                  })
+                }
+              </KeyboardLineContainer>
+            )
+          })
+        }
       </main>
     </PageContainer>
   )
